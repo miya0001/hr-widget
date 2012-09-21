6465bd256b39ff4744cfd79b9d649fb70a6a5c48
miya <miya@theta.ne.jp>
2012-09-22 04:56:19 +0900

    first commit

---
+++ hr-widget.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: Horizontal Rule Widget
+Description: This plugin allow you to insert <hr /> on sidebar widget.
+Author: Takayuki Miyauchi
+Version: 0.2
+Author URI: http://wpist.me/
+Plugin URI: http://wpist.me/
+Domain Path: /languages
+Text Domain: hr-widget
+*/
+
+
+class hrWidget extends WP_Widget {
+
+    private $num = 5;
+
+    function __construct() {
+		$widget_ops = array(
+            'description' => __(
+                'Insert horizontal rule.',
+                'hr-widget'
+            )
+        );
+		$control_ops = array();
+        parent::__construct(
+            false,
+            __('Horizontal Rule', 'hr-widget'),
+            $widget_ops,
+            $control_ops
+        );
+    }
+
+    public function widget($args, $instance) {
+
+        echo $args['before_widget'];
+        echo '<hr />';
+        echo $args['after_widget'];
+    }
+}
+
+class hrWidgetInit {
+
+function __construct()
+{
+    add_action('widgets_init', array(&$this, "widgets_init"));
+    add_action("plugins_loaded", array(&$this, "plugins_loaded"));
+}
+
+public function widgets_init()
+{
+    return register_widget("hrWidget");
+}
+
+public function plugins_loaded()
+{
+    load_plugin_textdomain(
+        "hr-widget",
+        false,
+        dirname(plugin_basename(__FILE__)).'/languages'
+    );
+}
+
+}
+
+new hrWidgetInit();
+
+?>
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Horizontal Rule Widget ===
+Contributors: miyauchi
+Donate link: http://wpist.me/
+Tags: widget, hr
+Requires at least: 3.2
+Tested up to: 3.4
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Insert hr (horizontal rule) tag to widget area.
+
+== Description ==
+
+Insert hr (horizontal rule) tag to widget area.
+
+[This plugin maintained on GitHub.](https://github.com/miya0001/hr-widget)
+
+
+= Translators =
+
+* Japanese(ja) - [Takayuki Miyauchi](http://firegoby.theta.ne.jp/)
+
+Please contact to me.
+
+* http://wpist.me/ (en)
+* http://firegoby.jp/ (ja)
+* @miya0001 on twitter.
+* https://github.com/miya0001/hr-widget
+
+= Contributors =
+
+* [Takayuki Miyauchi](http://wpist.me/)
+
+== Installation ==
+
+* A plug-in installation screen is displayed on the WordPress admin panel.
+* It installs it in `wp-content/plugins`.
+* The plug-in is made effective.
+
+== Changelog ==
+
+= 0.1.0 =
+* The first release.
+
+== Credits ==
+
+This plug-in is not guaranteed though the user of WordPress can freely use this plug-in free of charge regardless of the purpose.
+The author must acknowledge the thing that the operation guarantee and the support in this plug-in use are not done at all beforehand.
+
